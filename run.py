01d445b5ea532c921a2b3604313c57fb317a93c7
Ubuntu <ubuntu@ip-172-31-41-188.ap-south-1.compute.internal>
2020-03-12 05:11:08 +0000

    addded handlers

--- run.py
+++ run.py
@@ -14,9 +14,10 @@ user writes a text:
 """
 import os
 import shutil
+import json
 from flask import Flask, request
 
-from telegram import Bot
+from telegram import Bot, Update
 from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Filters
 
 
@@ -24,7 +25,7 @@ import logging
 logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.INFO)
 
 dispatcher = None
-audio_adapter = None
+bot = None
 application = Flask(__name__)
 
 from flask import Flask
@@ -33,6 +34,7 @@ app = Flask(__name__)
 BOT_TOKEN = "1054401279:AAESaTfz5nNuI6pgn3zbsDoSkj7LROSV0Ec"
 
 def setup_bot(token):
+    global bot
     bot = Bot(BOT_TOKEN)
     dispatcher = Dispatcher(bot, None, workers = 0)
 
@@ -42,23 +44,30 @@ def setup_bot(token):
 
     return dispatcher
 
-def handler_start():
-    return "hey"
+def handler_start(update, context):
+    #global bot
+    print("START INVOKED")
+    update.bot.send_message(chat_id = context.effective_chat.id, text = "got it buddy")
 
 
 @app.route("/", methods = ["POST"])
 def root_function():
-    msg = request.get_json()
+    global bot
+    msg = request.get_data()
     print("msg is ", msg)
+    decoded_msg = Update.de_json(json.loads(msg), bot)
+    print("decode msg is ", decoded_msg)
 
-    dispatcher.process_update(msg)
+    dispatcher.process_update(decoded_msg)
     
+    return json.dumps({"message" : "success", "statusCode" : 200})
 
 if __name__ == "__main__":
     dispatcher = setup_bot(BOT_TOKEN)
     
 
-    app.run(host = "0.0.0.0", port = "8080", ssl_context=('certificates/public.pem', 'certificates/private.key'), )
+    app.run(host = "0.0.0.0", port = "443", ssl_context=('certificates/public.pem', 'certificates/private.key'), debug=True)
+
 
 
 
